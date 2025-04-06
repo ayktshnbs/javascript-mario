5a23c457e79416459dadc9600a22d1eec7665a13
Aykut <ayktshnbs@gmail.com>
2025-04-06 16:22:39 +0300

    mario game first push

---
+++ game.js
@@ -0,0 +1,19 @@
+const { Client, Account, Databases, ID, Query } = Appwrite
+const projectId = '67f26e8a001ff5ee4d2d'
+const databaseId =''
+const collectionId = ''
+
+const client = new Client()
+    .setEndpoint('http://cloud.appwrite.com/v1')
+    .setProject(projectId)
+
+const account = new Account(client)
+
+function register (event) {
+    account.create(
+        ID.unique(),
+        event.target.elements ['register-email'].value,
+        event.target.elements ['register-password'].value,
+        event.target.elements ['register-username'].value,
+)
+}
